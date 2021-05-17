c650ea0a2fc1d3c4c5824b17ef6b67fc42a6133a
Valentin Abram <abram.valentin@outlook.com>
2021-05-17 07:42:18 +0300

    Get computer name dinamically

--- AdsAgregator.DAL/Database/AppDbContext.cs
+++ AdsAgregator.DAL/Database/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using AdsAgregator.DAL.Database.Tables;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.IO;
 
 namespace AdsAgregator.DAL.Database
@@ -17,7 +18,7 @@ namespace AdsAgregator.DAL.Database
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer("Server=tcp:adsagregatorserver.database.windows.net,1433;Initial Catalog=adsagregatordb;Persist Security Info=False;User ID=Boss;Password=hello123.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-64VPOK9;Initial Catalog=AdsAgregatorLocalDb;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False");
+            optionsBuilder.UseSqlServer($"Data Source={Environment.MachineName};Initial Catalog=AdsAgregatorLocalDb;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False");
             base.OnConfiguring(optionsBuilder);
         }
 
